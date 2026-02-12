0e212e9d999ad26d6a6b57bd95e57bf5013bd529
Justin Seo <carrier_mania@yahoo.com>
2026-02-12 11:09:32 -0800

    Initial Commit

---
+++ BabyTown/BabyTownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTownApp.swift
+//  BabyTown
+//
+//  Created by Justin Seo on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BabyTownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyTown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BabyTown
+//
+//  Created by Justin Seo on 2/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
