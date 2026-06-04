ccd6d21331bdb0c4a49f893d30c40880bfd1f9a0
Justin Seo <carrier_mania@yahoo.com>
2026-06-03 22:05:43 -0700

    feat(theme): add ColorTheme enum

---
+++ BabyTown/Theme/ColorTheme.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// The two selectable app color themes. Default is `.pink` (the original look).
+enum ColorTheme: String, CaseIterable, Codable {
+    case pink
+    case blue
+}
